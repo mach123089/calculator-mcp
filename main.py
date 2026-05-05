42a266d8c73696fadc15d954d5b2768ff6b36365
mach123089 <mach123089437691@gmail.com>
2026-05-05 07:52:39 +0200

    fix add main block

--- main.py
+++ main.py
@@ -24,3 +24,6 @@ def div_float(a: float, b: float) -> float:
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+if __name__ == "__main__":                                                                  
+    mcp.run(transport="stdio")
